0cd878bddfed818ac82faf1f0593b35e7456b59c
MICLEM <miclemabasie3@gmail.com>
2022-04-08 03:47:54 +0100

    modified scrollbar

--- components/SearchFilters.jsx
+++ components/SearchFilters.jsx
@@ -2,7 +2,6 @@ import { useEffect, useState } from 'react'
 import { Flex, Select, Box, Text, Input, Spinner, Icon, Button} from '@chakra-ui/react'
 import { useRouter } from 'next/router'
 import { MdCancel } from 'react-icons/md'
-import Image from 'next/image'
 import {filterData, getFilterValues } from '../utils/filterData'
 import router from 'next/router'
 
